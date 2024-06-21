cd023126aa5eeb6373ca5016123d9e829e502145
hanjiabao <andrew.han@imile.me>
2024-06-21 14:35:48 +0800

    整理pom

--- memory-chat-memory/src/main/java/Application.java
+++ memory-chat-memory/src/main/java/Application.java
@@ -1,6 +1,5 @@
 import com.github.hambuger.memory.chat.wechat.service.LoginService;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -16,11 +15,9 @@ public class Application {
 
     private static ConfigurableApplicationContext context;
 
+
     public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-        System.out.println("start 1");
         context = new SpringApplicationBuilder(Application.class).headless(false).run();
-        System.out.println("start 2");
         LoginService loginService = context.getBean(LoginService.class);
         loginService.login();
 
--- memory-chat-wechat/src/main/java/com/github/hambuger/memory/chat/wechat/entity/Contacts.java
+++ memory-chat-wechat/src/main/java/com/github/hambuger/memory/chat/wechat/entity/Contacts.java
@@ -7,7 +7,6 @@ import java.util.Objects;
 
 import javax.swing.*;
 
-import cn.afterturn.easypoi.excel.annotation.Excel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,105 +23,74 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Contacts {
-    @Excel(name="用户名",width = 50)
-    private String username;
+        private String username;
 
-    @Excel(name="昵称",width = 20)
-    private String nickname;
+        private String nickname;
 
-    @Excel(name="签名",width = 50)
-    private String signature;
+        private String signature;
 
-    @Excel(name="性别",replace ={"男_1","女_2"} )
-    private Byte sex;
+        private Byte sex;
 
-    @Excel(name="备注",width = 20)
-    private String remarkname;
+        private String remarkname;
 
-    @Excel(name="省份")
-    private String province;
+        private String province;
 
-    @Excel(name="城市")
-    private String city;
+        private String city;
 
-    @Excel(name="chatroomid")
-    private Double chatroomid;
+        private Double chatroomid;
 
-    @Excel(name="attrstatus")
-    private Double attrstatus;
+        private Double attrstatus;
 
     /**
      * 等于0消息免打扰 1正常
      */
-    @Excel(name="statues")
-    private Double statues;
+        private Double statues;
 
-    @Excel(name="pyquanpin")
-    private String pyquanpin;
+        private String pyquanpin;
 
-    @Excel(name="encrychatroomid")
-    private String encrychatroomid;
+        private String encrychatroomid;
 
-    @Excel(name="displayname")
-    private String displayname;
+        private String displayname;
 
-    @Excel(name="verifyflag")
-    private Integer verifyflag;
+        private Integer verifyflag;
 
-    @Excel(name="unifriend")
-    private Double unifriend;
+        private Double unifriend;
 
-    @Excel(name="contactflag")
-    private Double contactflag;
+        private Double contactflag;
 
     //@ExcelCollection(name="群成员")
     private List<Contacts> memberlist;
 
-    @Excel(name="starfriend")
-    private Double starfriend;
+        private Double starfriend;
 
-    @Excel(name="headimgurl")
-    private String headimgurl;
+        private String headimgurl;
 
-    @Excel(name="appaccountflag")
-    private Double appaccountflag;
+        private Double appaccountflag;
 
-    @Excel(name="membercount")
-    private Double membercount;
+        private Double membercount;
 
-    @Excel(name="remarkpyinitial")
-    private String remarkpyinitial;
+        private String remarkpyinitial;
 
 
-    @Excel(name="snsflag")
-    private Double snsflag;
+        private Double snsflag;
 
-    @Excel(name="alias")
-    private String alias;
+        private String alias;
 
-    @Excel(name="keyword")
-    private String keyword;
+        private String keyword;
 
-    @Excel(name="hideinputbarflag")
-    private Double hideinputbarflag;
+        private Double hideinputbarflag;
 
-    @Excel(name="remarkpyquanpin")
-    private String remarkpyquanpin;
+        private String remarkpyquanpin;
 
-    @Excel(name="uin")
-    private Double uin;
+        private Double uin;
 
-    @Excel(name="owneruin")
-    private Double owneruin;
+        private Double owneruin;
 
-    @Excel(name="isowner")
-    private Double isowner;
+        private Double isowner;
 
-    @Excel(name="pyinitial")
-    private String pyinitial;
+        private String pyinitial;
 
-    @Excel(name="ticket")
-    private String ticket;
+        private String ticket;
 
 
     /**
