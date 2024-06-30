998eb443c9f9fe33fffce962e9bb703ebc73d682
hamburger <hanjbde@163.com>
2024-06-30 20:43:22 +0800

    fix

--- memory-chat-memory/src/main/java/com/github/hambuger/memory/chat/memory/chat/ChatCompletionsApi.java
+++ memory-chat-memory/src/main/java/com/github/hambuger/memory/chat/memory/chat/ChatCompletionsApi.java
@@ -276,7 +276,7 @@ public class ChatCompletionsApi {
         SpringAiChatMessageMemoryDTO memoryDTO = BeanUtil.copyProperties(baseMemoryDTO, SpringAiChatMessageMemoryDTO.class);
         memoryDTO.setMessageId(IdUtil.generateUniqueId());
         memoryDTO.setMessageCreatorId(memoryDTO.getMessageCreatorName());
-        memoryDTO.setMessageCreatorType(StringUtils.equals(YES_STR, baseMemoryDTO.getGroupMsgFlag()) ? CreatorEnum.USER.getType() : CreatorEnum.GROUP.getType());
+        memoryDTO.setMessageCreatorType(StringUtils.equals(YES_STR, baseMemoryDTO.getGroupMsgFlag()) ? CreatorEnum.GROUP.getType() : CreatorEnum.USER.getType());
         memoryDTO.setMessageReceiveId(CreatorEnum.Andrew.getUserId());
         memoryDTO.setMessageReceiveName(CreatorEnum.Andrew.getUserName());
         memoryDTO.setMessageReceiveType(CreatorEnum.Andrew.getType());
