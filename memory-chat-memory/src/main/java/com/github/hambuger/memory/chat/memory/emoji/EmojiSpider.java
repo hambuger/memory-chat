c8b46809c3ab3ac40ddc26590f4690f85e01219f
andrew.han <andrew.han@imile.me>
2024-07-15 14:34:43 +0800

    使用另一个表情搜索网站

--- memory-chat-memory/src/main/java/com/github/hambuger/memory/chat/memory/emoji/EmojiSpider.java
+++ memory-chat-memory/src/main/java/com/github/hambuger/memory/chat/memory/emoji/EmojiSpider.java
@@ -1,99 +1,92 @@
 package com.github.hambuger.memory.chat.memory.emoji;
 
-/**
- * @author hanjiabao
- * @since 2024/7/2
- */
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.github.hambuger.memory.chat.memory.util.MyHttpUtils;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 
-import org.apache.commons.collections4.CollectionUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import lombok.extern.slf4j.Slf4j;
 
+
+/**
+ * @author hanjiabao
+ * @since 2024/7/15
+ */
 
 @Slf4j
 @Component
 public class EmojiSpider {
 
-    @Value("${emoji.baseUrlPrefix}")
-    private String baseUrl;
-
-    @Value("${emoji.urlContent}")
-    private String urlContent;
-
-    @Value("${emoji.userAgent}")
-    private String userAgent;
 
     @Value("${emoji.directory}")
     private String directory;
 
+    // 网址
+    private final static String emojiWebAddress = "https://www.dbbqb.com/api/search/json?start=0&w=/%s";
 
-    public String searchEmoji(String word) {
-        String searchUrl = baseUrl + String.format(urlContent, word);
 
+    public String searchEmoji(String keyword) {
         try {
-            // 获取页面内容
-            Document doc = Jsoup.connect(searchUrl).userAgent(userAgent).get();
-            if (Files.notExists(Paths.get(directory))) {
-                Files.createDirectories(Paths.get(directory));
-            }
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Accept", "application/json");
+            headers.put("Accept-Encoding", "gzip, deflate, br, zstd");
+            headers.put("Accept-Language", "zh-CN,zh;q=0.9");
+            headers.put("Connection", "keep-alive");
+            headers.put("Content-Type", "application/json");
+            headers.put("Host", "www.dbbqb.com");
+            headers.put("Referer", String.format("https://www.dbbqb.com/s?w=/%s", keyword));
+            headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
+            headers.put("Web-Agent", "web");
 
-            // 解析图片信息
-            Elements imgList = doc.select("img.ui.image.bqppsearch.lazy");
-            if (CollectionUtils.isEmpty(imgList)) {
-                return null;
-            }
-            Element img = imgList.get(0);
-            String imgUrl = img.attr("data-original");
-//            String imgTitle = img.attr("title");
-            try {
-                // 构造图片保存路径
-                String extension = imgUrl.substring(imgUrl.lastIndexOf("."));
-                String filePath = directory + UUID.randomUUID() + extension;
 
-                // 下载图片
-                downloadImage(imgUrl, filePath);
-                return filePath;
-            } catch (Exception e) {
-                log.warn("searchEmoji error", e);
+            String response = MyHttpUtils.get(String.format(emojiWebAddress, keyword), headers, null);
+            JSONArray array = JSONArray.parseArray(response);
+            if (!array.isEmpty()) {
+                JSONObject object = array.getJSONObject(0);
+                String url = "https://image.dbbqb.com/" + object.getString("path");
+                String downUrl = directory + keyword;
+                return downloadImage(url, downUrl);
             }
-
         } catch (Exception e) {
-            log.warn("searchEmoji error", e);
+            log.error("searchEmoji error", e);
         }
         return null;
     }
 
 
-    // 下载图片方法
-    private void downloadImage(String imageUrl, String destinationFilePath) throws Exception {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("User-Agent", userAgent);
-        headers.put("Referer", baseUrl);
-        HttpResponse<InputStream> response = Unirest.get(imageUrl).queryString(null).headers(headers).asBinary();
-        try (InputStream in = response.getBody(); FileOutputStream out = new FileOutputStream(destinationFilePath)) {
+    /**
+     * 下载图片到本地
+     */
+    private String downloadImage(String imageUrl, String destinationFilePath) throws Exception {
+        HttpResponse<InputStream> response = Unirest.get(imageUrl).asBinary();
+        try {
+            List<String> contentTypes = response.getHeaders().get("Content-Type");
+            if (contentTypes != null && contentTypes.size() > 0) {
+                destinationFilePath = destinationFilePath + contentTypes.get(0).replace("image/", ".");
+            }
+            InputStream in = response.getBody();
+            FileOutputStream out = new FileOutputStream(destinationFilePath);
             byte[] buffer = new byte[4096];
             int bytesRead;
             while ((bytesRead = in.read(buffer)) != -1) {
                 out.write(buffer, 0, bytesRead);
             }
+            return destinationFilePath;
+        } catch (Exception e) {
+            log.error("downloadImage error", e);
         }
+        return destinationFilePath;
     }
 }
 
---
+++ memory-chat-memory/src/main/java/com/github/hambuger/memory/chat/memory/emoji/EmojiSpider1.java
@@ -0,0 +1,99 @@
+package com.github.hambuger.memory.chat.memory.emoji;
+
+/**
+ * @author hanjiabao
+ * @since 2024/7/2
+ */
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Component
+public class EmojiSpider1 {
+
+    @Value("${emoji.baseUrlPrefix}")
+    private String baseUrl;
+
+    @Value("${emoji.urlContent}")
+    private String urlContent;
+
+    @Value("${emoji.userAgent}")
+    private String userAgent;
+
+    @Value("${emoji.directory}")
+    private String directory;
+
+
+    public String searchEmoji(String word) {
+        String searchUrl = baseUrl + String.format(urlContent, word);
+
+        try {
+            // 获取页面内容
+            Document doc = Jsoup.connect(searchUrl).userAgent(userAgent).get();
+            if (Files.notExists(Paths.get(directory))) {
+                Files.createDirectories(Paths.get(directory));
+            }
+
+            // 解析图片信息
+            Elements imgList = doc.select("img.ui.image.bqppsearch.lazy");
+            if (CollectionUtils.isEmpty(imgList)) {
+                return null;
+            }
+            Element img = imgList.get(0);
+            String imgUrl = img.attr("data-original");
+            //            String imgTitle = img.attr("title");
+            try {
+                // 构造图片保存路径
+                String extension = imgUrl.substring(imgUrl.lastIndexOf("."));
+                String filePath = directory + UUID.randomUUID() + extension;
+
+                // 下载图片
+                downloadImage(imgUrl, filePath);
+                return filePath;
+            } catch (Exception e) {
+                log.warn("searchEmoji error", e);
+            }
+
+        } catch (Exception e) {
+            log.warn("searchEmoji error", e);
+        }
+        return null;
+    }
+
+
+    // 下载图片方法
+    private void downloadImage(String imageUrl, String destinationFilePath) throws Exception {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("User-Agent", userAgent);
+        headers.put("Referer", baseUrl);
+        HttpResponse<InputStream> response = Unirest.get(imageUrl).queryString(null).headers(headers).asBinary();
+        try (InputStream in = response.getBody(); FileOutputStream out = new FileOutputStream(destinationFilePath)) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
+
